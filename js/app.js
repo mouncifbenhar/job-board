6709145259b31ede99b39ba7bf9f03f4847bc699
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-09-23 19:25:46 +0000

    feat: add EventListener to get input value and activ searsh

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 import { offers } from "./data.js"
 import { disply_pagination} from "./render.js"
-
+import { search as sertsh} from "./filters.js"
 
 disply_pagination(offers)
+const search = document.querySelector("#search")
+search.addEventListener("input",()=>{
+   const result = sertsh(offers,search.value)
+   disply_pagination(result)
+})
+
+
 
