05cf21079a6aed9810e6844633383b428272928a
mouncifbenhar <mouncifbenhar7@gmail.com>
2026-09-23 10:35:44 +0000

    feat: add async function getData

---
+++ js/data.js
@@ -0,0 +1,11 @@
+export async function getData() {
+
+    try{
+        const response = await fetch("../data/offers.json")
+        const data = response.json()
+        return data
+    }catch(error){
+        return []
+    }
+    
+}
